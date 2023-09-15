9ac5a06ab770e41cb1602fe48840955ce7592859
hugo <hgk9872@gmail.com>
2023-09-15 17:54:39 +0900

    findByRoomId 메서드 추가

---
+++ ChatServer/src/main/java/com/modu/ChatServer/repository/ChatMessageRepository.java
@@ -0,0 +1,10 @@
+package com.modu.ChatServer.repository;
+
+import com.modu.ChatServer.domain.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+    List<ChatMessage> findByRoomId(String roomId);
+}
