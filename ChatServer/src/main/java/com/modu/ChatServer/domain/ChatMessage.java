94df9c57059a57b8b100a79cc42756430f92c7f0
hugo <hgk9872@gmail.com>
2023-09-15 17:55:25 +0900

    mongodb용 Document 객체

---
+++ ChatServer/src/main/java/com/modu/ChatServer/domain/ChatMessage.java
@@ -0,0 +1,28 @@
+package com.modu.ChatServer.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@Document(collection = "message")
+public class ChatMessage {
+
+    @Id
+    @Field("_id")
+    private String id;
+
+    @Field("room_id")
+    private String roomId;
+    private String writer;
+    private String message;
+
+    @CreatedDate
+    @Field("created_at")
+    private LocalDateTime createdAt;
+}
