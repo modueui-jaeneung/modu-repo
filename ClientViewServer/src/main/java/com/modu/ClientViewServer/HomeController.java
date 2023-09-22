737b065ad7b06f9e6f38054a5dcf5eb1f4dfd3d3
hugo <hgk9872@gmail.com>
2023-09-22 14:23:51 +0900

    feat: 클라이언트 <-> ChatServer 연동 테스트
    - 클라이언트에서 채팅 관련 클래스를 chat 폴더로 옮김
    - view 수정
    
    Related to : #13

--- ClientViewServer/src/main/java/com/modu/ClientViewServer/HomeController.java
+++ ClientViewServer/src/main/java/com/modu/ClientViewServer/HomeController.java
@@ -52,43 +52,4 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("/chat")
-    public String chatIndex(Model model) {
-
-        String url = "http://localhost:8085/chat/rooms";
-        ResponseEntity<ChatRoomDto[]> response = restTemplate.getForEntity(url, ChatRoomDto[].class);
-
-        ChatRoomDto[] rooms = response.getBody();
-        model.addAttribute("rooms", rooms);
-
-        return "chat";
-    }
-
-    @GetMapping("/chat/{roomId}")
-    public String enterRoom(@PathVariable String roomId, Model model) {
-        String url = "http://localhost:8085/chat";
-
-        // 채팅방 목록 가져오기
-        ResponseEntity<ChatRoomDto[]> responseRooms = restTemplate.getForEntity(url + "/rooms", ChatRoomDto[].class);
-
-        ChatRoomDto[] rooms = responseRooms.getBody();
-        model.addAttribute("rooms", rooms);
-
-        // RoomID에 해당하는 채팅방 가져오기
-        ChatRoomDto room = restTemplate.getForObject(url + "/rooms/" + roomId, ChatRoomDto.class);
-        model.addAttribute("room", room);
-
-//        // 메시지 가져오기
-        ResponseEntity<ChatMessageDto[]> responseMessages =
-                restTemplate.getForEntity(url + "/rooms/" + roomId + "/messages", ChatMessageDto[].class);
-
-        ChatMessageDto[] messages = responseMessages.getBody();
-        model.addAttribute("messages", messages);
-
-        // TODO : 실제 사용자 닉네임과 연동 생각하기. 현재는 입장할 때 username이 랜덤으로 생성됨
-        model.addAttribute("username", UUID.randomUUID().toString());
-
-        return "room";
-    }
-
 }
--- ClientViewServer/src/main/java/com/modu/ClientViewServer/chat/ChatMessageDto.java
+++ ClientViewServer/src/main/java/com/modu/ClientViewServer/chat/ChatMessageDto.java
@@ -2,10 +2,13 @@ package com.modu.ClientViewServer.chat;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class ChatMessageDto {
 
     private String roomId;
     private String writer;
     private String message;
+    private LocalDateTime createdAt;
 }
