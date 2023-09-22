b824fd7dcf87ea70efe747be9f1cf29fbd772dfd
hugo <hgk9872@gmail.com>
2023-09-22 14:01:30 +0900

    feat: ChatRoomDto 구현
    - ChatRoomDto 구현 및
    - 클라이언트에 응답하는 부분을 ChatRoom -> ChatRoomDto로 변경
    - redisTemplate 의 json serialize 문제 해결을 위한 의존성 추가 - com.fasterxml.jackson.datatype:jackson-datatype-jsr310
    - ChatRoomDto의 LocalDateTime 필드에 직렬화 문제 해결을 위한 어노테이션 추가
    
    Related to : #12

--- ChatServer/src/main/java/com/modu/ChatServer/dto/ChatMessageDto.java
+++ ChatServer/src/main/java/com/modu/ChatServer/dto/ChatMessageDto.java
@@ -2,7 +2,6 @@ package com.modu.ChatServer.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.modu.ChatServer.domain.ChatMessage;
